22e365ba5b8063421f63677ea2c449fb744bc1e5
goreleaserbot <goreleaser@carlosbecker.com>
2018-09-24 23:53:36 -0300

    Brew formula update for shadowfox-updater version v1.6.0

--- Formula/shadowfox-updater.rb
+++ Formula/shadowfox-updater.rb
@@ -1,9 +1,9 @@
 class ShadowfoxUpdater < Formula
   desc "An automatic updater for ShadowFox"
   homepage "https://github.com/SrKomodo/shadowfox-updater"
-  url "https://github.com/SrKomodo/shadowfox-updater/releases/download/v1.5.9/shadowfox-updater_1.5.9_MacOS_x64.tar.gz"
-  version "1.5.9"
-  sha256 "cbb653a463c3c18763686aeec5a7343c31b602bf5b91be9fe977e3e41b8c73de"
+  url "https://github.com/SrKomodo/shadowfox-updater/releases/download/v1.6.0/shadowfox-updater_1.6.0_MacOS_x64.tar.gz"
+  version "1.6.0"
+  sha256 "33406f90001b9d35cf33717aa9d4846c89ace83f9acd96a2f6fa01428eb68721"
 
   def install
     bin.install "shadowfox"
