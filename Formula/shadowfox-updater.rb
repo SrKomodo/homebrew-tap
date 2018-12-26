ac179d40945181be0e738ea64bc7d4d2dd1aabb4
Travis CI User <travis@example.org>
2018-12-26 23:04:43 +0000

    "v1.7.18"

--- Formula/shadowfox-updater.rb
+++ Formula/shadowfox-updater.rb
@@ -1,9 +1,9 @@
 class ShadowfoxUpdater < Formula
   desc "An automatic updater for ShadowFox"
   homepage "https://github.com/SrKomodo/shadowfox-updater"
-  url "https://github.com/SrKomodo/shadowfox-updater/releases/download/v1.7.17/shadowfox_mac_x64"
-  version "v1.7.17"
-  sha256 "12eda523598c7af382792c549545233d82bfafac187162ff5309c567b8f42596"
+  url "https://github.com/SrKomodo/shadowfox-updater/releases/download/v1.7.18/shadowfox_mac_x64"
+  version "v1.7.18"
+  sha256 ""
 
   def install
     File.rename("shadowfox_mac_x64", "shadowfox")
