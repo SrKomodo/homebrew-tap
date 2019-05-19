be792639228c97785d5d33a741487aa7c2b8599f
SrKomodo <komodosr@gmail.com>
2019-05-18 23:50:21 -0300

    Travis pls

--- Formula/shadowfox-updater.rb
+++ Formula/shadowfox-updater.rb
@@ -3,7 +3,7 @@ class ShadowfoxUpdater < Formula
   homepage "https://github.com/SrKomodo/shadowfox-updater"
   url "https://github.com/SrKomodo/shadowfox-updater/releases/download/v2.0.0/shadowfox_mac_x64"
   version "v2.0.0"
-  sha256 ""
+  sha256 "22464e89b0b080e31a3c81ee64fa24ea97a5ed4cb926a868807ee0fd7a8bc5c7"
 
   def install
     File.rename("shadowfox_mac_x64", "shadowfox")
